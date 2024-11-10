391429b0310239406d3e93baf9d265217237c861
Harsh Joshi <harsh.joshi@users.noreply.github.com>
2024-11-10 15:04:42 -0700

    Make Caller Library Per resource operation

--- DbxPbiApiWrapper/DbxPbiWrapper.py
+++ DbxPbiApiWrapper/DbxPbiWrapper.py
@@ -1,7 +1,7 @@
 from BaseValueObjects import *
 from BaseApiHelper import *
 
-class PbiValueObject:
+class PbiDatasetValueObject:
     def __init__(self):
         self.Token = None
         self.ValueGroup = None
@@ -13,7 +13,7 @@ class PbiValueObject:
         self.RefreshJson = None   
         self.ValueDatasetUsers = None     
         
-class IPbiValueBuilder(metaclass= ABCMeta):
+class IPbiDatasetValueBuilder(metaclass= ABCMeta):
     
     @property
     @abstractmethod
@@ -59,9 +59,9 @@ class IPbiValueBuilder(metaclass= ABCMeta):
     def getUsersInDataset(self, groupName, datasetName) -> None:
         pass
 
-class PbiValueBuilder(IPbiValueBuilder):
+class PbiDatasetValueBuilder(IPbiDatasetValueBuilder):
     def __init__(self, tenant, accountKey, accountSecret):
-        self.PbiValueObject = PbiValueObject()
+        self.PbiDatasetValueObject = PbiDatasetValueObject()
         self.accountKey = accountKey
         self.accountSecret = accountSecret
         self.tenant = tenant
@@ -71,51 +71,51 @@ class PbiValueBuilder(IPbiValueBuilder):
     def getRefreshJson(self):
         jsonCreator = f"""{{ 'type': 'Full' }}        
                         """      
-        self.PbiValueObject.RefreshJson = jsonCreator
+        self.PbiDatasetValueObject.RefreshJson = jsonCreator
         return self
     
     def getSafeAadToken(self):
         tokenHelper = BaseApiHelper.TokenHelper(self.tenant, self.accountKey, self.accountSecret)
-        self.PbiValueObject.Token = tokenHelper.getValidatedAADToken(self.PbiValueObject.Token)
-        print(f"GetSafeToken = {self.PbiValueObject.Token}")
+        self.PbiDatasetValueObject.Token = tokenHelper.getValidatedAADToken(self.PbiDatasetValueObject.Token)
+        print(f"GetSafeToken = {self.PbiDatasetValueObject.Token}")
         return self
     
     def getGroups(self):
-        self.PbiValueObject.ValueGroups = self.pbiApiHelper.getGroup(self.PbiValueObject.Token, groupName = None)        
+        self.PbiDatasetValueObject.ValueGroups = self.pbiApiHelper.getGroup(self.PbiDatasetValueObject.Token, groupName = None)        
         return self
 
     def getGroupId(self, groupName):
-        self.PbiValueObject.ValueGroup = self.pbiApiHelper.getGroup(self.PbiValueObject.Token, groupName = groupName)
-        print(f"GetGroup = {self.PbiValueObject.ValueGroup}")
+        self.PbiDatasetValueObject.ValueGroup = self.pbiApiHelper.getGroup(self.PbiDatasetValueObject.Token, groupName = groupName)
+        print(f"GetGroup = {self.PbiDatasetValueObject.ValueGroup}")
         return self
     
     def getDatasets(self):
-        self.PbiValueObject.ValueDatasets = self.pbiApiHelper.getDataset(self.PbiValueObject.Token, self.PbiValueObject.ValueGroup, datasetName=None)
+        self.PbiDatasetValueObject.ValueDatasets = self.pbiApiHelper.getDataset(self.PbiDatasetValueObject.Token, self.PbiDatasetValueObject.ValueGroup, datasetName=None)
         return self
 
     def getDatasetId(self, datasetName):
-        self.PbiValueObject.ValueDataset = self.pbiApiHelper.getDataset(self.PbiValueObject.Token, self.PbiValueObject.ValueGroup, datasetName=datasetName)
-        print(f"GetDataset = {self.PbiValueObject.ValueDataset}")
+        self.PbiDatasetValueObject.ValueDataset = self.pbiApiHelper.getDataset(self.PbiDatasetValueObject.Token, self.PbiDatasetValueObject.ValueGroup, datasetName=datasetName)
+        print(f"GetDataset = {self.PbiDatasetValueObject.ValueDataset}")
         return self
     
     def existingRefresh(self):        
-        refreshRunning = self.pbiApiHelper.refreshInProgress(self.PbiValueObject.Token, self.PbiValueObject.ValueGroup, self.PbiValueObject.ValueDataset)
+        refreshRunning = self.pbiApiHelper.refreshInProgress(self.PbiDatasetValueObject.Token, self.PbiDatasetValueObject.ValueGroup, self.PbiDatasetValueObject.ValueDataset)
         print(f"Existing refresh running status = {refreshRunning}")
         return refreshRunning
 
     def xmlaPostRequest(self):
-        if(self.PbiValueObject.RefreshJson == None):
+        if(self.PbiDatasetValueObject.RefreshJson == None):
             print("No partitions to refresh. Skipping request to refresh.")
             return self
-        print(f"Refreshing GroupId: {self.PbiValueObject.ValueGroup.id} and DatasetId: {self.PbiValueObject.ValueDataset.id}")
-        self.PbiValueObject.ApiResponseObject = self.pbiApiHelper.refreshDataset(self.PbiValueObject.Token, self.PbiValueObject.ValueGroup, self.PbiValueObject.ValueDataset, self.PbiValueObject.RefreshJson)                
+        print(f"Refreshing GroupId: {self.PbiDatasetValueObject.ValueGroup.id} and DatasetId: {self.PbiDatasetValueObject.ValueDataset.id}")
+        self.PbiDatasetValueObject.ApiResponseObject = self.pbiApiHelper.refreshDataset(self.PbiDatasetValueObject.Token, self.PbiDatasetValueObject.ValueGroup, self.PbiDatasetValueObject.ValueDataset, self.PbiDatasetValueObject.RefreshJson)                
         return self
     
     def getUsersInDataset(self):
-        self.PbiValueObject.ValueDatasetUsers = self.pbiApiHelper.getUsersInDataset(self.PbiValueObject.Token, self.PbiValueObject.ValueGroup, self.PbiValueObject.ValueDataset)
+        self.PbiDatasetValueObject.ValueDatasetUsers = self.pbiApiHelper.getUsersInDataset(self.PbiDatasetValueObject.Token, self.PbiDatasetValueObject.ValueGroup, self.PbiDatasetValueObject.ValueDataset)
         return self
 
-class DbxPbiWrapper:
+class DbxPbiDatasetWrapper:
     def __init__(self, tenant, accountKey, accountSecret) -> None:
         self.tenant = tenant
         self.accountKey = accountKey
@@ -123,7 +123,7 @@ class DbxPbiWrapper:
         
 
     def refreshPbiDataset(self,workspaceName, datasetName):
-        builder = PbiValueBuilder(self.tenant, self.accountKey, self.accountSecret)
+        builder = PbiDatasetValueBuilder(self.tenant, self.accountKey, self.accountSecret)
         builder = builder.getSafeAadToken()
         builder = builder.getGroupId(workspaceName)
         builder = builder.getDatasetId(datasetName)
@@ -140,22 +140,22 @@ class DbxPbiWrapper:
         print(f"API Call completed with following result {builder.PbiRefresh.ApiResponseObject}")
     
     def getAllDatasetsInWorkspace(self, workspaceName):
-        builder = PbiValueBuilder(self.tenant, self.accountKey, self.accountSecret)
+        builder = PbiDatasetValueBuilder(self.tenant, self.accountKey, self.accountSecret)
         builder = builder.getSafeAadToken()
         builder = builder.getGroupId(workspaceName)
         builder =  builder.getDatasets()
-        return builder.PbiValueObject.ValueDatasets
+        return builder.PbiDatasetValueObject.ValueDatasets
     
     def getAllWorkspaces(self):
-        builder = PbiValueBuilder(self.tenant, self.accountKey, self.accountSecret)
+        builder = PbiDatasetValueBuilder(self.tenant, self.accountKey, self.accountSecret)
         builder = builder.getSafeAadToken()
         builder =  builder.getGroups()        
-        return builder.PbiValueObject.ValueGroups
+        return builder.PbiDatasetValueObject.ValueGroups
     
     def getUsersInDataset(self, workspaceName, datasetName):
-        builder = PbiValueBuilder(self.tenant, self.accountKey, self.accountSecret)
+        builder = PbiDatasetValueBuilder(self.tenant, self.accountKey, self.accountSecret)
         builder = builder.getSafeAadToken()
         builder = builder.getGroupId(workspaceName)
         builder = builder.getDatasetId(datasetName)
         builder = builder.getUsersInDataset()
-        return builder.PbiValueObject.ValueDatasetUsers
+        return builder.PbiDatasetValueObject.ValueDatasetUsers
