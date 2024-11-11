f9103f74c59dd7fa66efb93d3ed7709e18e7365a
Harsh Joshi <114756037+HarshJos@users.noreply.github.com>
2024-11-11 16:55:03 +0000

    Update Version Number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description="Dbx to Pbi Wrapper",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.0.8",
+    version="0.0.9",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
